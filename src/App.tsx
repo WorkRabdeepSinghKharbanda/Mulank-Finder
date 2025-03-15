8e175e435381488112b52b59b6bd9f55b284f80b
Rabdeep singh kharbanda <rabdeep.s@uniqode.com>
2025-03-16 02:23:48 +0530

    minor update

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import './styles/App.module.scss';
 // Components
 import { Hero, MulankSummarizer, MulankWidget }  from './Components/index'
-import { Fragment } from 'react/jsx-runtime';
 function App() {
   return (
    <>
